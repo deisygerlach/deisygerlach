1c16c943dde5a0b515a7de5a2601925e311a6540
SamuelVegaLIT <samuel.vega@liderazgointegral.co>
2026-03-15 16:12:10 -0500

    Change path

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  basePath: "",
-  assetPrefix: "",
+  basePath: "/deisygerlach",
+  assetPrefix: "/deisygerlach/",
   trailingSlash: true,
 
   images: {
